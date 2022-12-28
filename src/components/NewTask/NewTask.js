703c9a96c83a811dfedd9dd23394a09aaea2fa10
Alexandre Franca <Alexandre.Franca@bjss.com>
2022-12-28 13:15:01 +0000

    feat: using useHttp in the add Task page. Two solutions here, the one using bind() was implemented, the other one is commented out.

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,39 @@
-import { useState } from 'react';
+import React from 'react';
+import useHttp from '../../hooks/use-http';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+    // Transform from Firebase response to required format
+    const createTask = (taskText, taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+    };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-72a7f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    const requestConfig = {
+      url: 'https://react-http-72a7f-default-rtdb.europe-west1.firebasedatabase.app/tasks.json', 
+      method: 'POST',
+      body: { text: taskText },
+      headers: {
+        'Content-Type': 'application/json',
+      },      
     }
-    setIsLoading(false);
+    sendTaskRequest(requestConfig, createTask.bind(null, taskText));
+  
+    /*
+    // Transform from Firebase response to required format
+    const createTask = (taskData) => {
+      const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+    };
+    sendTaskRequest(requestConfig, createTask);
+    */
   };
 
   return (
